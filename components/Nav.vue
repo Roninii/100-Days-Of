0aae7babf4539f0c119438e8288e741da501f571
Ronnie Villarini <Villarini91@gmail.com>
2020-07-20 11:53:38 -0500

    add branding, add some communities

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,14 +1,16 @@
 <template>
-  <nav
-    class="grid grid-flow-col gap-4 justify-end items-baseline text-gray-600 font-medium uppercase leading-4 px-8 py-4"
-  >
-    <a v-if="atHome" class="hover:text-purple-500" href="/#about">Learn More</a>
-    <nuxt-link v-else class="hover:text-purple-500" to="/"> Home</nuxt-link>
-    <nuxt-link class="hover:text-purple-500" to="/communities"
-      >Communities</nuxt-link
-    >
-    <nuxt-link class="hover:text-purple-500" to="/login">Sign In</nuxt-link>
-  </nav>
+  <header class="flex justify-between font-medium uppercase leading-4 text-gray-600 px-8 py-4">
+    <div class="flex items-baseline">
+      <nuxt-link class="hover:text-purple-500 text-2xl" to="/">#100Days</nuxt-link>
+    </div>
+
+    <nav class="grid grid-flow-col gap-4 justify-end items-baseline">
+      <a v-if="atHome" class="hover:text-purple-500" href="/#about">Learn More</a>
+      <nuxt-link v-else class="hover:text-purple-500" to="/">Home</nuxt-link>
+      <nuxt-link class="hover:text-purple-500" to="/communities">Communities</nuxt-link>
+      <nuxt-link class="hover:text-purple-500" to="/login">Sign In</nuxt-link>
+    </nav>
+  </header>
 </template>
 
 <script lang="ts">
--- components/homepage/About.vue
+++ components/homepage/About.vue
@@ -1,25 +1,18 @@
 <template>
   <section id="about" class="bg-purple-500">
     <header class="text-center">
-      <h2 class="text-4xl text-white font-medium uppercase">
-        About The challenge
-      </h2>
-      <h3 class="text-lg text-purple-100">
-        100 days of what?! Are you crazy?!
-      </h3>
+      <h2 class="text-4xl text-white font-medium uppercase">About The challenge</h2>
+      <h3 class="text-lg text-purple-100">100 days of what?! Are you crazy?!</h3>
     </header>
 
-    <section
-      class="max-w-screen-sm mx-auto py-8 text-white leading-normal text-xl"
-    >
+    <section class="max-w-screen-sm mx-auto py-8 text-white leading-normal text-xl">
       <p>
         I promise you, I'm not. An
         <a
           href="https://twitter.com/search?q=%23100DaysOf&src=typed_query"
           class="underline font-bold"
           target="_blank"
-          >absurd</a
-        >
+        >absurd</a>
         amount of people take part in various 100 days of [x] challenges, and
         for good reason!
       </p>
@@ -28,22 +21,23 @@
           You'll have plenty of opportunity to learn a new skill, or just deepen
           your knowledge of one!
         </li>
-        <li>
-          You can create a new, healthy, and maintainable habbit!
-        </li>
-        <li>
-          Everyone says Practice makes perfect, and there is plenty of proof!
-        </li>
+        <li>You can create a new, healthy, and maintainable habbit!</li>
+        <li>Everyone says Practice makes perfect, and there is plenty of proof!</li>
       </ul>
 
       <p>
         There are loads of reasons, but the best way to learn more is to dive in
         yourself. So think of something you really want to learn or just work
         on, set your goal,
-        <nuxt-link class="underline" to="/communities"
-          >join a community</nuxt-link
-        >, and get started!
+        <nuxt-link class="underline" to="/communities">join a community</nuxt-link>, and get started!
       </p>
+
+      <div class="flex justify-center my-4">
+        <nuxt-link
+          to="/sign-up"
+          class="border-1 border-purple-500 bg-purple-500 rounded px-4 py-2 text-white text-center uppercase underline hover:bg-purple-400 hover:border-purple-400 transition duration-300 linear"
+        >I'm Ready</nuxt-link>
+      </div>
 
       <div class="flex justify-around pt-8">
         <img
@@ -55,13 +49,13 @@
     </section>
 
     <footer class="relative">
-      <div class="absolute -top-16 left-0 -z-1">
+      <div class="absolute -top-16 left-0 right-0 -z-1">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#9f7aea "
             fill-opacity="1"
             d="M0,224L120,229.3C240,235,480,245,720,250.7C960,256,1200,256,1320,256L1440,256L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
-          ></path>
+          />
         </svg>
       </div>
     </footer>
