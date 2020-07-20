deb4caa3a42da58c1dc694210da9bf26bc0df350
Ronnie Villarini <Villarini91@gmail.com>
2020-07-20 15:58:58 -0500

    bind event listeners to allow v-model and other events

--- components/base/BaseInput.vue
+++ components/base/BaseInput.vue
@@ -3,45 +3,59 @@
     <label
       v-if="label"
       :for="label"
-      class="absolute text-sm text-gray-600 uppercase"
+      :class="['absolute', 'text-sm', 'text-gray-600', 'uppercase, inline']"
       >{{ label }}</label
     >
     <input
       :id="label ? label : null"
       v-bind="attrs"
+      v-bind:value="value"
+      v-on="eventListeners"
       class="border-b-2 outline-none border-purple-500 w-full"
     />
   </div>
 </template>
 
 <script>
-import { defineComponent, toRefs } from '@vue/composition-api'
+import { defineComponent, toRefs, computed } from '@vue/composition-api'
 
 export default defineComponent({
-  props: {
-    label: {
-      type: String,
-      required: false,
-    },
-  },
+  props: ['label', 'value'],
   setup(props, ctx) {
-    const { attrs } = ctx
+    const { attrs, listeners, emit } = ctx
+
+    const eventListeners = computed(() => {
+      return Object.assign(
+        {},
+        // `Object.assign` merges objects together to form a new object
+        listeners,
+        // Then we can add custom listeners or override the
+        // behavior of some listeners.
+        {
+          // This ensures that the component works with v-model
+          input: function (event) {
+            emit('input', event.target.value)
+          },
+        }
+      )
+    })
 
     return {
       attrs,
+      eventListeners,
     }
   },
 })
 </script>
 
 <style scoped>
-label {
+.inline {
   bottom: 5px;
   transform: translate3d(); /* enable hardware acceleration */
   transition: all 200ms linear;
 }
 
-.base-input:focus-within label {
+.base-input:focus-within .inline {
   bottom: 2rem;
   @apply text-xs;
 }
