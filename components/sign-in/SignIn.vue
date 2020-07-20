fc3d8298122509bcefb8069d14487d157b6ed69e
Ronnie Villarini <Villarini91@gmail.com>
2020-07-20 14:09:59 -0500

    add google sign in button

--- components/sign-in/SignIn.vue
+++ components/sign-in/SignIn.vue
@@ -1,17 +1,22 @@
 <template>
   <BaseCard title="Login with email and password">
-    <form class="grid gap-10 py-8 leading-relaxed text-lg">
+    <form class="grid gap-10 py-8 leading-relaxed text-lg" @submit.prevent="">
       <BaseInput type="email" label="email" />
       <BaseInput type="password" label="password" />
-      <div class="flex justify-center">
+      <div class="grid grid-flow-col gap-2">
         <BaseButton type="submit" label="Log In" />
+        <BaseButton type="button" label="Google Sign In" />
       </div>
     </form>
   </BaseCard>
 </template>
 
 <script>
-export default {}
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  methods: {},
+})
 </script>
 
 <style scoped></style>
