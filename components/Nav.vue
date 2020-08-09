7837d8f6cfa4fd4e0d970215f5bca2081c610f9f
Ronnie Villarini <Villarini91@gmail.com>
2020-08-09 17:11:03 -0500

    add mobile nav stylings

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,53 +2,73 @@
   <header
     class="flex justify-between font-medium uppercase leading-4 text-gray-600 px-8 py-4"
   >
-    <div class="flex items-baseline">
+    <div class="flex items-center">
       <nuxt-link class="hover:text-purple-500 text-2xl" to="/"
         >#100Days</nuxt-link
       >
     </div>
 
-    <nav class="grid grid-flow-col gap-4 justify-end items-baseline">
-      <a v-if="atHome" class="hover:text-purple-500" href="/#about"
-        >Learn More</a
-      >
-      <nuxt-link v-else class="hover:text-purple-500" to="/">Home</nuxt-link>
-      <nuxt-link class="hover:text-purple-500" to="/communities"
-        >Communities</nuxt-link
-      >
-
-      <!-- Sign in / out -->
+    <nav class="relative">
       <button
-        v-if="isLoggedIn"
-        class="font-medium uppercase leading-4 text-gray-600 hover:text-purple-500"
-        @click="logout"
+        class="md:hidden p-4"
+        type="button"
+        name="menu"
+        @click="toggleMenu"
       >
-        Sign Out
+        {{ menuHidden ? 'Menu' : 'X' }}
       </button>
-      <nuxt-link v-else class="hover:text-purple-500" to="/login"
-        >Sign In</nuxt-link
+      <div
+        class="absolute right-0 text-right bg-white p-4 md:p-0 shadow-lg md:shadow-none rounded-b-md md:rounded-none md:text-center md:relative grid md:grid md:grid-flow-col gap-4 justify-end items-baseline"
+        :class="{ hidden: menuHidden }"
       >
+        <a v-if="atHome" class="hover:text-purple-500" href="/#about"
+          >Learn More</a
+        >
+        <nuxt-link v-else class="hover:text-purple-500" to="/">Home</nuxt-link>
+
+        <nuxt-link class="hover:text-purple-500" to="/dashboard"
+          >Dashboard</nuxt-link
+        >
+
+        <nuxt-link class="hover:text-purple-500" to="/communities"
+          >Communities</nuxt-link
+        >
+
+        <!-- Sign in / out -->
+        <button
+          v-if="isLoggedIn"
+          class="font-medium uppercase leading-4 text-gray-600 hover:text-purple-500 text-right"
+          @click="logout"
+        >
+          Sign Out
+        </button>
+        <nuxt-link v-else class="hover:text-purple-500 text-right" to="/login"
+          >Sign In</nuxt-link
+        >
+      </div>
     </nav>
   </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, computed, ref } from '@vue/composition-api'
 
 export default defineComponent({
-  computed: {
-    atHome() {
-      return this.$route.path === '/'
-    },
-    isLoggedIn() {
-      return !!this.$store.state.user.currentUser
-    },
-  },
+  setup(props, { root: { $router, $route, $store, $fireAuth } }) {
+    const atHome = computed(() => $route.path === '/')
+    const isLoggedIn = computed(() => !!$store.state.user.currentUser)
+    const menuHidden = ref(true)
+    const logout = () => $fireAuth.signOut
 
-  methods: {
-    logout() {
-      this.$fireAuth.signOut()
-    },
+    const toggleMenu = () => (menuHidden.value = !menuHidden.value)
+
+    return {
+      atHome,
+      isLoggedIn,
+      logout,
+      menuHidden,
+      toggleMenu,
+    }
   },
 })
 </script>
