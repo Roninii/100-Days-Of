5e6ff76f11c67ef61170d69dc727904e328b8cae
= <=>
2020-08-18 22:02:58 -0500

    error handling for failed fetching of qod

--- components/dashboard/Quote.vue
+++ components/dashboard/Quote.vue
@@ -1,5 +1,8 @@
 <template>
-  <blockquote class="text-center">
+  <p v-if="error" class="text-center text-gray-600">
+    There was an issue fetching the quote of the day ):
+  </p>
+  <blockquote v-else class="text-center">
     <p class="text-2xl">{{ quote }}</p>
     <footer class="text-gray-600">-{{ author }}</footer>
   </blockquote>
@@ -12,19 +15,26 @@ import {
   onMounted,
   reactive,
   toRefs,
+  ref,
 } from '@vue/composition-api'
 
 export default defineComponent({
   setup() {
     const quote = reactive({ author: '', quote: '' })
+    const error = ref(false)
     onMounted(async () => {
-      const qod = await getQuote()
-      quote.author = qod.author
-      quote.quote = qod.quote
+      try {
+        const qod = await getQuote()
+        quote.author = qod.author
+        quote.quote = qod.quote
+      } catch (e) {
+        error.value = true
+      }
     })
 
     return {
       ...toRefs(quote),
+      error,
     }
   },
 })
