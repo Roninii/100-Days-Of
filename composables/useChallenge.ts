e0addbf4ca16bdd5cf0bf9de4c101e76b4ad0dff
= <=>
2020-08-28 22:08:31 -0500

    add logging modal

--- composables/useChallenge.ts
+++ composables/useChallenge.ts
@@ -1,66 +1,74 @@
 type Challenge = {
-    id: string;
-    name: string;
-    homepage?: string;
-    start?: object;
-    paused?: boolean;
-    pausedDays?: number | undefined;
+  id: string;
+  name: string;
+  homepage?: string;
+  start?: object;
+  paused?: boolean;
+  pausedDays?: number | undefined;
 };
 
 export function useChallenge(ctx: any) {
-    const {
-        root: { $fireStore, $store, $router },
-    } = ctx;
-    const userRef = $fireStore.doc(`users/${$store.state.user.currentUser.id}`);
+  const {
+    root: { $fireStore, $store, $router },
+  } = ctx;
+  const userRef = $fireStore.doc(`users/${$store.state.user.currentUser.id}`);
 
-    const joinChallenge = async (challenge: Challenge) => {
-        const user = await userRef.get();
+  const joinChallenge = async (challenge: Challenge) => {
+    const user = await userRef.get();
 
-        if (user.exists) {
-            $store.dispatch('user/joinChallenge', { userRef, challenge });
-            $router.push({ path: '/dashboard' });
-        }
+    if (user.exists) {
+      $store.dispatch("user/joinChallenge", { userRef, challenge });
+      $router.push({ path: "/dashboard" });
+    }
+  };
+
+  const leaveChallenge = async (challenge: Challenge) => {
+    const user = await userRef.get();
+
+    if (user.exists) {
+      $store.dispatch("user/leaveChallenge", { userRef, challenge });
+    }
+  };
+
+  const pauseChallenge = async (challenge: Challenge) => {
+    const user = await userRef.get();
+
+    const challengeUpdate = {
+      ...challenge,
+      paused: true,
+      pausedDays: challenge?.pausedDays ?? 1,
     };
 
-    const leaveChallenge = async (challenge: Challenge) => {
-        const user = await userRef.get();
+    if (user.exists) {
+      $store.dispatch("user/pauseChallenge", {
+        userRef,
+        challenge,
+        challengeUpdate,
+      });
+    }
+  };
 
-        if (user.exists) {
-            $store.dispatch('user/leaveChallenge', { userRef, challenge });
-        }
+  const unpauseChallenge = async (challenge: Challenge) => {
+    const user = await userRef.get();
+
+    const challengeUpdate = {
+      ...challenge,
+      paused: false,
     };
 
-    const pauseChallenge = async (challenge: Challenge) => {
-        const user = await userRef.get();
+    if (user.exists) {
+      $store.dispatch("user/unpauseChallenge", {
+        userRef,
+        challenge,
+        challengeUpdate,
+      });
+    }
+  };
 
-        const challengeUpdate = {
-            ...challenge,
-            paused: true,
-            pausedDays: challenge?.pausedDays ?? 1,
-        };
-
-        if (user.exists) {
-            $store.dispatch('user/pauseChallenge', { userRef, challenge, challengeUpdate });
-        }
-    };
-
-    const unpauseChallenge = async (challenge: Challenge) => {
-        const user = await userRef.get();
-
-        const challengeUpdate = {
-            ...challenge,
-            paused: false,
-        };
-
-        if (user.exists) {
-            $store.dispatch('user/unpauseChallenge', { userRef, challenge, challengeUpdate });
-        }
-    };
-
-    return {
-        joinChallenge,
-        leaveChallenge,
-        pauseChallenge,
-        unpauseChallenge,
-    };
+  return {
+    joinChallenge,
+    leaveChallenge,
+    pauseChallenge,
+    unpauseChallenge,
+  };
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,101 +1,102 @@
 export default {
-    /*
-     ** Nuxt rendering mode
-     ** See https://nuxtjs.org/api/configuration-mode
-     */
-    mode: 'spa',
-    /*
-     ** Nuxt target
-     ** See https://nuxtjs.org/api/configuration-target
-     */
-    target: 'static',
-    /*
-     ** Headers of the page
-     ** See https://nuxtjs.org/api/configuration-head
-     */
-    head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            {
-                hid: 'description',
-                name: 'description',
-                content: process.env.npm_package_description || '',
-            },
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    },
-    /*
-     ** Global CSS
-     */
-    css: ['@/assets/css/main.css'],
-    /*
-     ** Plugins to load before mounting the App
-     ** https://nuxtjs.org/guide/plugins
-     */
-    plugins: ['@/plugins/composition-api.ts', '@/plugins/vuelidate.ts'],
-    /*
-     ** Auto import components
-     ** See https://nuxtjs.org/api/configuration-components
-     */
-    components: true,
-    /*
-     ** Nuxt.js dev-modules
-     */
-    buildModules: [
-        '@nuxt/typescript-build',
-        // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/color-mode',
-        '@nuxtjs/moment',
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "spa",
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: "static",
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-        // Doc: https://axios.nuxtjs.org/usage
-        '@nuxtjs/axios',
-        '@nuxtjs/pwa',
-        '@nuxtjs/firebase',
-    ],
-    firebase: {
-        config: {
-            apiKey: 'AIzaSyB1KxXuIzBmSpZuk1Z8YXIcK_6dmLNIMUI',
-            authDomain: 'days-of-a5da8.firebaseapp.com',
-            databaseURL: 'https://days-of-a5da8.firebaseio.com',
-            projectId: 'days-of-a5da8',
-            storageBucket: 'days-of-a5da8.appspot.com',
-            messagingSenderId: '971937063349',
-            appId: '1:971937063349:web:2164a8901b4fa70baf79fc',
-        },
-        services: {
-            auth: {
-                initialize: {
-                    onAuthStateChangedAction: 'user/updateCurrentUser',
-                },
-            },
-            firestore: true,
-        },
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  /*
+   ** Global CSS
+   */
+  css: ["@/assets/css/main.css"],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: ["@/plugins/composition-api.ts", "@/plugins/vuelidate.ts"],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    "@nuxt/typescript-build",
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/moment",
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
+    "@nuxtjs/firebase",
+    "portal-vue/nuxt",
+  ],
+  firebase: {
+    config: {
+      apiKey: "AIzaSyB1KxXuIzBmSpZuk1Z8YXIcK_6dmLNIMUI",
+      authDomain: "days-of-a5da8.firebaseapp.com",
+      databaseURL: "https://days-of-a5da8.firebaseio.com",
+      projectId: "days-of-a5da8",
+      storageBucket: "days-of-a5da8.appspot.com",
+      messagingSenderId: "971937063349",
+      appId: "1:971937063349:web:2164a8901b4fa70baf79fc",
     },
-    /*
-     ** Axios module configuration
-     ** See https://axios.nuxtjs.org/options
-     */
-    axios: {},
-    /*
-     ** Build configuration
-     ** See https://nuxtjs.org/api/configuration-build/
-     */
-    build: {},
+    services: {
+      auth: {
+        initialize: {
+          onAuthStateChangedAction: "user/updateCurrentUser",
+        },
+      },
+      firestore: true,
+    },
+  },
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
 
-    router: {
-        extendRoutes(routes, resolve) {
-            routes.push({
-                path: '/login',
-                component: resolve(__dirname, 'pages/Login.vue'),
-                alias: '/sign-up',
-            });
-        },
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        path: "/login",
+        component: resolve(__dirname, "pages/Login.vue"),
+        alias: "/sign-up",
+      });
     },
+  },
 };
