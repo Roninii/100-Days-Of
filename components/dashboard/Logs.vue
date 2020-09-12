954085b045ea4a12ac9221362265d17b47469738
Ronnie Villarini <>
2020-09-11 19:06:06 -0500

    move date to computed function

--- components/dashboard/Logs.vue
+++ components/dashboard/Logs.vue
@@ -1,25 +1,29 @@
 <template>
-  <BaseCard title="Recent Logs">
-    <aside class="grid gap-4 py-8 overflow-y-scroll">
-      <Log :log="log" />
-    </aside>
-  </BaseCard>
+    <BaseCard title="Recent Logs">
+        <aside class="grid gap-4 py-8 overflow-y-scroll">
+            <Log :log="log" />
+        </aside>
+    </BaseCard>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
-  name: "Logs",
-  data() {
-    return {
-      log: {
-        challenge: "#100DaysOfCode",
-        date: this.$moment().format("l"),
-        message:
-          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quos numquam rerum harum in hic libero! Suscipit vel itaque labore. ",
-      },
-    };
-  },
+    name: 'Logs',
+    data() {
+        return {
+            log: {
+                challenge: '#100DaysOfCode',
+                message:
+                    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quos numquam rerum harum in hic libero! Suscipit vel itaque labore. ',
+            },
+        };
+    },
+    computed: {
+        date() {
+            return this.$moment().format('l');
+        },
+    },
 });
 </script>
